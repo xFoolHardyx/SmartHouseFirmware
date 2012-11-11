fed3a4e7f22f70c51a395992e80ea66b1ef062e2
Sergey Busygin <sergey.busygin.1990@gmail.com>
2012-11-11 23:45:27 +0200

    mod LastHope home

--- RTOS_last_hope/SRC/main.c
+++ RTOS_last_hope/SRC/main.c
@@ -1,5 +1,22 @@
+#include <settings.h>
+#include <AT91SAM7X256.h>
+
 void main (void)
 {
-	for (;;){}
+	InitFrec();
+	InitPerepherial();
+
+	void delay (unsigned long a) {while (--a!=0);}
+
+	const AT91PS_PIO m_pPIOA = AT91C_BASE_PIOA;
+	for (;;)
+	{
+		ClrBIT(m_pPIOA,BIT17);
+		SetBIT(m_pPIOA,BIT18);
+		delay(800000);             //simple delay
+		ClrBIT(m_pPIOA,BIT18);
+		SetBIT(m_pPIOA,BIT17);
+		delay(800000);             //simple delay
+	}
 }
 
